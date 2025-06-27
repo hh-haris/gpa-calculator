18a4f4085f040bdc7c18f926d28ce03a7c7cffe8
Haris Habib <harishabib.me@gmail.com>
2025-06-27 19:21:05 +0500

    Updated SpinningText.tsx

--- src/components/SpinningText.tsx
+++ src/components/SpinningText.tsx
@@ -53,8 +53,8 @@ function SpinningText() {
               ease: 'linear'
             }}
             style={{
-              width: '140px',
-              height: '140px',
+              width: '100px',
+              height: '100px',
             }}
           >
             {letters.map((letter, index) => (
