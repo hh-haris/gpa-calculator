d87fb79e5f7ba43715b6da1294452419f0af81d4
Haris Habib <harishabib.me@gmail.com>
2025-06-27 19:17:02 +0500

    Updated SpinningText.tsx

--- src/components/SpinningText.tsx
+++ src/components/SpinningText.tsx
@@ -38,7 +38,7 @@ function SpinningText() {
       {isVisible && (
         <motion.div
           className="fixed top-4 right-4 z-50"
-          initial={{ opacity: 0, scale: 0.8 }}
+          initial={{ opacity: 0, scale: 0.9 }}
           animate={{ opacity: 1, scale: 1 }}
           exit={{ opacity: 0, scale: 0.8 }}
           transition={{ duration: 0.3 }}
@@ -82,4 +82,3 @@ function SpinningText() {
 }
 
 export default SpinningText;
-
