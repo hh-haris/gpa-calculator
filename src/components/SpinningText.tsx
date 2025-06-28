5ec2ae66b7c7bea9054438323ac1124d958bead4
Haris Habib <harishabib.me@gmail.com>
2025-06-28 13:29:22 +0500

    Updated SpinningText.tsx

--- src/components/SpinningText.tsx
+++ src/components/SpinningText.tsx
@@ -65,7 +65,7 @@ function SpinningText() {
                   top: '50%',
                   transform: `
                     rotate(${(360 / total) * index}deg)
-                    translateY(-4.25em)
+                    translateY(-3em)
                   `,
                   transformOrigin: 'center',
                 }}
